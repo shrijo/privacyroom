28605b45506ade53e783c92506f8e542f4094aa4
jshriqui <jshriqui@esri.com>
2020-10-16 17:45:06 +0200

    New var

--- js/stepper-listener.js
+++ js/stepper-listener.js
@@ -5,8 +5,9 @@ AFRAME.registerComponent('stepper-listener', {
   tick: function () {
 
     var cam = document.querySelector("#cam");
+    var position = new THREE.Vector3();
 
   //  console.log("hello");
-    console.log(cam.el.getWorldPosition(position));
+    console.log(cam.object3D.getWorldPosition(position));
   }
 });
