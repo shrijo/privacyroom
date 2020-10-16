9f05baef066c3fef1ee1adaf2f70df5c52d14b67
jshriqui <jshriqui@esri.com>
2020-10-16 18:21:29 +0200

    Distance

--- js/stepper-listener.js
+++ js/stepper-listener.js
@@ -5,9 +5,17 @@ AFRAME.registerComponent('stepper-listener', {
   tick: function () {
 
     var cam = document.querySelector("#cam");
-    var position = new THREE.Vector3();
+    var positionPlayer = new THREE.Vector3();
+    var positionSelf = new THREE.Vector3();
+
+
+
+    cam.object3D.getWorldPosition(positionPlayer));
+    this.el.object3D.getWorldPosition(positionSelf);
+
+    var distance = positionSelf.distanceTo( positionPlayer );
 
   //  console.log("hello");
-    console.log(this.el.object3D.distanceTo(cam.object3D.getWorldPosition(position)));
+    console.log(distance);
   }
 });
