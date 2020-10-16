bb471ad6371f0ec4394fb4a18597bb66d22f5cbb
jshriqui <jshriqui@esri.com>
2020-10-16 18:52:34 +0200

    ==

--- js/stepper-listener.js
+++ js/stepper-listener.js
@@ -20,7 +20,7 @@ AFRAME.registerComponent('stepper-listener', {
 
     var distance = Math.sqrt( dx * dx + dy * dy );
 
-    if (distance < 0.5 && isOn=false) {
+    if (distance < 0.5 && isOn==false) {
       isOn = true;
       this.el.emit('hover');
     }
