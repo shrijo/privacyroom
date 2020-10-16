14e5d99d56b69edf480e1bd1d65481708a5812b2
jshriqui <jshriqui@esri.com>
2020-10-16 17:41:22 +0200

    Within tick

--- js/stepper-listener.js
+++ js/stepper-listener.js
@@ -1,9 +1,12 @@
 AFRAME.registerComponent('stepper-listener', {
 
-  //var cam = document.querySelector("#cam");
+
 
   tick: function () {
-    console.log("hello");
-  //  console.log(cam.el.object3D.getWorldPosition(position));
+
+    var cam = document.querySelector("#cam");
+
+  //  console.log("hello");
+    console.log(cam.el.object3D.getWorldPosition(position));
   }
 });
