4b8bb1f69afc30e58589e0e0d7ebd738875db24c
jshriqui <jshriqui@esri.com>
2020-10-16 18:30:03 +0200

    Out of function

--- js/stepper-listener.js
+++ js/stepper-listener.js
@@ -7,22 +7,19 @@ AFRAME.registerComponent('stepper-listener', {
     var cam = document.querySelector("#cam");
     var positionPlayer = new THREE.Vector3();
     var positionSelf = new THREE.Vector3();
+    var distance
 
 
 
     cam.object3D.getWorldPosition(positionPlayer);
     this.el.object3D.getWorldPosition(positionSelf);
 
+    var dx = positionPlayer.x - positionSelf.x;
+    var dy = positionPlayer.y - positionSelf.y;
 
-    function distanceVector( positionPlayer, positionSelf )
-    {
-        var dx = positionPlayer.x - positionSelf.x;
-        var dy = positionPlayer.y - positionSelf.y;
-
-        return Math.sqrt( dx * dx + dy * dy );
-    }
+    var distance = Math.sqrt( dx * dx + dy * dy );
 
   //  console.log("hello");
-    console.log(distanceVector);
+    console.log(distance);
   }
 });
