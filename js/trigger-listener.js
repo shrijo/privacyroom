0d769f5c4d11d9926f942efa06e07188b24d597c
jshriqui <jshriqui@esri.com>
2020-10-17 12:51:05 +0200

    Check parent

--- js/trigger-listener.js
+++ js/trigger-listener.js
@@ -1,31 +1,12 @@
 AFRAME.registerComponent('trigger-listener', {
 
+  init: function () {
+    var parentIsOn = false;
+  },
+
   tick: function () {
-
-    var camera = document.querySelector("#cam");
-    var positionPlayer = new THREE.Vector3();
-    var positionSelf = new THREE.Vector3();
-    //var triggeredPost = this.el.dataset.trigger;
-
-    var distance;
-
-    positionPlayer.setFromMatrixPosition(camera.object3D.matrixWorld);
-
-    this.parentNode.el.object3D.getWorldPosition(positionSelf);
-
-    var dx = positionPlayer.x - positionSelf.x;
-    var dy = positionPlayer.z - positionSelf.z;
-
-    var distance = Math.sqrt( dx * dx + dy * dy );
-
-    if (distance < 0.5 && this.el.classList.contains("isOff")) {
-      this.el.classList.replace("isOff", "isOn")
-      this.el.emit('hover');
-    }
-
-    else if (distance >= 0.5 && this.el.classList.contains("isOn")) {
-      this.el.classList.replace("isOn", "isOff")
-      this.el.emit('leave');
+    if (this.parentNode.classList.contains('isOn')){
+      alert('hello')
     }
   }
 });
