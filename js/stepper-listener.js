9671429457346408553ae956259b0dce947c55e2
jshriqui <jshriqui@esri.com>
2020-10-17 12:00:19 +0200

    El

--- js/stepper-listener.js
+++ js/stepper-listener.js
@@ -21,13 +21,13 @@ AFRAME.registerComponent('stepper-listener', {
     if (distance < 0.5 && this.el.classList.contains("isOff")) {
       this.el.classList.replace("isOff", "isOn")
       this.el.emit('hover');
-      document.getElementById(triggeredPost).emit('hover');
+      document.getElementById(triggeredPost).el.emit('hover');
     }
 
     else if (distance >= 0.5 && this.el.classList.contains("isOn")) {
       this.el.classList.replace("isOn", "isOff")
       this.el.emit('leave');
-      document.getElementById(triggeredPost).emit('leave');
+      document.getElementById(triggeredPost).el.emit('leave');
     }
   }
 });
