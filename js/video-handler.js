d21f481cf1acda081b7329ea96fb1ad7742a91a4
shrijo <jonas.shriqui@gmail.com>
2020-10-31 13:02:00 +0100

    play

--- js/video-handler.js
+++ js/video-handler.js
@@ -2,12 +2,6 @@ AFRAME.registerComponent('video-handler',{
   init: functon(){
    let el = this.el;
    let vid = document.querySelector("#myvideo");
-   vid.pause();
-   el.addEventListener('mouseenter',function(){
-      vid.play();
-   });
-   el.addEventListener('mouseleave',function(){
-      vid.pause();
-   });
+   vid.play();
   }
 });
